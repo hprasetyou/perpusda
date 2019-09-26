211449b1abc98b831e3ed3aa34437654a01cf34a
Heru Prasetyo Utomo <hprasetyou@gmail.com>
2019-09-26 21:13:16 +0700

    image uploader

--- resources/js/res/config.js
+++ resources/js/res/config.js
@@ -1,2 +1,2 @@
-export const API_CLIENT_ID = '2';
-export const API_CLIENT_SECRET = 'cQZjLgOyBAJKTLzjGAmCJErjUgeFvx0wCDwflmS4';
+export const API_CLIENT_ID = '1';
+export const API_CLIENT_SECRET = '7LwJzVo98pRVm2N4xC3RcVlM2bCsfEveAHo3nrNG';
